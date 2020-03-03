0a2ce93ff38df21b8b0fc6b1229fa314fef8fb37
Himanshu Bansal <himanshu.kte@gmail.com>
2020-03-04 01:40:31 +0530

    initial commit

--- index.php
+++ index.php
@@ -1,45 +1,42 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<!-- Google Fonts -->
-	<link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
-	<!-- Styling for public area -->
-	<link rel="stylesheet" href="static/css/public_styling.css">
-	<meta charset="UTF-8">
-	<title>LifeBlog | Home </title>
+<?php require_once('config.php') ?>
+<?php require_once( ROOT_PATH . '/includes/public_functions.php') ?>
+
+<!-- Retrieve all posts from database  -->
+<?php $posts = getPublishedPosts(); ?>
+<?php require_once( ROOT_PATH . '/includes/head_section.php') ?>
+<title>AWS Blog | Home </title>
 </head>
 <body>
 	<!-- container - wraps whole page -->
 	<div class="container">
-		<!-- navbar -->
-		<div class="navbar">
-			<div class="logo_div">
-				<a href="index.php"><h1>AWS Blog</h1></a>
-			</div>
-			<ul>
-			  <li><a class="active" href="index.php">Home</a></li>
-			  <li><a href="#news">News</a></li>
-			  <li><a href="#contact">Contact</a></li>
-			  <li><a href="#about">About</a></li>
-			</ul>
-		</div>
+		<!-- navbar --> 
+		<?php include( ROOT_PATH . '/includes/navbar.php') ?>
 		<!-- // navbar -->
+
+		<!-- banner -->
+		<?php include( ROOT_PATH . '/includes/banner.php') ?>
 
 		<!-- Page content -->
 		<div class="content">
 			<h2 class="content-title">Recent Articles</h2>
 			<hr>
 			<!-- more content still to come here ... -->
+			<?php foreach ($posts as $post): ?>
+	             <div class="post" style="margin-left: 0px;">
+		         <img src="<?php echo BASE_URL . '/static/images/' . $post['image']; ?>" class="post_image" alt="">
+		         <a href="single_post.php?post-slug=<?php echo $post['slug']; ?>">
+		    	<div class="post_info">
+				<h3><?php echo $post['title'] ?></h3>
+				<div class="info">
+					<span><?php echo date("F j, Y ", strtotime($post["created_at"])); ?></span>
+					<span class="read_more">Read more...</span>
+				</div>
+			</div>
+		</a>
+	</div>
+<?php endforeach ?>
 		</div>
 		<!-- // Page content -->
 
 		<!-- footer -->
-		<div class="footer">
-			<p>MyViewers &copy; <?php echo date('Y'); ?></p>
-		</div>
-		<!-- // footer -->
-
-	</div>
-	<!-- // container -->
-</body>
-</html>
+		<?php include( ROOT_PATH . '/includes/footer.php') ?>
